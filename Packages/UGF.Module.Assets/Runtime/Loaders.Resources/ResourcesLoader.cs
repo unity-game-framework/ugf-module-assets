e4cc19bb36c121b73272d07cac71b4da9cb3f3fb
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-24 18:09:12 +0300

    Add ResourcesLoader

---
+++ Packages/UGF.Module.Assets/Runtime/Loaders.Resources/ResourcesLoader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UGF.Module.Assets.Runtime.Loaders.Resources
+{
+    public class ResourcesLoader : AssetLoaderBase
+    {
+        public bool ProviderAssetUnload { get; }
+
+        public ResourcesLoader(bool providerAssetUnload = false)
+        {
+            ProviderAssetUnload = providerAssetUnload;
+        }
+
+        protected override async Task<object> OnLoad(IAssetProvider provider, string id, Type type)
+        {
+            IAssetGroup group = provider.GetGroupByAsset(id);
+            IAssetInfo assetInfo = group.GetInfo(id);
+
+            ResourceRequest request = UnityEngine.Resources.LoadAsync(assetInfo.Address, type);
+
+            while (!request.isDone)
+            {
+                await Task.Yield();
+            }
+
+            return request.asset;
+        }
+
+        protected override Task OnUnload(IAssetProvider provider, string id, object asset)
+        {
+            if (ProviderAssetUnload)
+            {
+                if (!(asset is Object unityAsset)) throw new ArgumentException($"Asset must be a Unity Object to unload: '{asset}'.");
+
+                UnityEngine.Resources.UnloadAsset(unityAsset);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Packages/UGF.Module.Assets/Runtime/Loaders.Resources/ResourcesLoaderAsset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UGF.Module.Assets.Runtime.Loaders.Resources
+{
+    public class ResourcesLoaderAsset : AssetLoaderAssetBase
+    {
+        [SerializeField] private bool m_provideAssetUnload;
+
+        public bool ProvideAssetUnload { get { return m_provideAssetUnload; } set { m_provideAssetUnload = value; } }
+
+        protected override IAssetLoader OnBuild()
+        {
+            return new ResourcesLoader(m_provideAssetUnload);
+        }
+    }
+}
