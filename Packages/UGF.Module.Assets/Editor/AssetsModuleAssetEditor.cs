88ce4e5b9ddb388dd5a71de47352759823aeb6f9
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-26 22:44:38 +0300

    Add editors

---
+++ Packages/UGF.Module.Assets/Editor/AssetsModuleAssetEditor.cs
@@ -0,0 +1,64 @@
+﻿using UGF.Application.Editor;
+using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.AssetReferences;
+using UGF.Module.Assets.Runtime;
+using UnityEditor;
+
+namespace UGF.Module.Assets.Editor
+{
+    [CustomEditor(typeof(AssetsModuleAsset), true)]
+    internal class AssetsModuleAssetEditor : ApplicationModuleAssetEditor
+    {
+        private SerializedProperty m_propertyScript;
+        private SerializedProperty m_propertyUnloadTrackedAssetsOnUninitialize;
+        private AssetReferenceListDrawer m_listLoaders;
+        private AssetReferenceListDrawer m_listGroups;
+        private ReorderableListDrawer m_listPreloadAssets;
+        private ReorderableListDrawer m_listPreloadAssetsAsync;
+
+        private void OnEnable()
+        {
+            m_propertyScript = serializedObject.FindProperty("m_Script");
+            m_propertyUnloadTrackedAssetsOnUninitialize = serializedObject.FindProperty("m_unloadTrackedAssetsOnUninitialize");
+
+            m_listLoaders = new AssetReferenceListDrawer(serializedObject.FindProperty("m_loaders"));
+            m_listGroups = new AssetReferenceListDrawer(serializedObject.FindProperty("m_groups"));
+            m_listPreloadAssets = new ReorderableListDrawer(serializedObject.FindProperty("m_preload"));
+            m_listPreloadAssetsAsync = new ReorderableListDrawer(serializedObject.FindProperty("m_preloadAsync"));
+
+            m_listLoaders.Enable();
+            m_listGroups.Enable();
+            m_listPreloadAssets.Enable();
+            m_listPreloadAssetsAsync.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listLoaders.Disable();
+            m_listGroups.Disable();
+            m_listPreloadAssets.Disable();
+            m_listPreloadAssetsAsync.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.UpdateIfRequiredOrScript();
+
+            using (new EditorGUI.DisabledScope(true))
+            {
+                EditorGUILayout.PropertyField(m_propertyScript);
+            }
+
+            EditorGUILayout.PropertyField(m_propertyUnloadTrackedAssetsOnUninitialize);
+
+            m_listLoaders.DrawGUILayout();
+            m_listGroups.DrawGUILayout();
+            m_listPreloadAssets.DrawGUILayout();
+            m_listPreloadAssetsAsync.DrawGUILayout();
+
+            serializedObject.ApplyModifiedProperties();
+
+            DrawModuleRegisterTypeInfo();
+        }
+    }
+}
---
+++ Packages/UGF.Module.Assets/Editor/Loaders.Resources/ResourcesAssetGroupAssetListDrawer.cs
@@ -0,0 +1,38 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Assets.Editor.Loaders.Resources
+{
+    internal class ResourcesAssetGroupAssetListDrawer : ReorderableListDrawer
+    {
+        public ResourcesAssetGroupAssetListDrawer(SerializedProperty serializedProperty) : base(serializedProperty)
+        {
+        }
+
+        protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty propertyId = serializedProperty.FindPropertyRelative("m_id");
+            SerializedProperty propertyAddress = serializedProperty.FindPropertyRelative("m_address");
+
+            AttributeEditorGUIUtility.DrawResourcesPathField(position, propertyAddress, GUIContent.none, typeof(Object));
+
+            Object asset = UnityEngine.Resources.Load(propertyAddress.stringValue);
+            string path = AssetDatabase.GetAssetPath(asset);
+            string guid = AssetDatabase.AssetPathToGUID(path);
+
+            propertyId.stringValue = guid;
+        }
+
+        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        protected override bool OnElementHasVisibleChildren(SerializedProperty serializedProperty)
+        {
+            return false;
+        }
+    }
+}
