a969ea72ba727f932f2372ef2bc5ed6a9a31611b
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-26 21:10:50 +0300

    Update TestAssetsModule.cs

--- Assets/UGF.Module.Assets.Runtime.Tests/TestAssetsModule.cs
+++ Assets/UGF.Module.Assets.Runtime.Tests/TestAssetsModule.cs
@@ -9,6 +9,27 @@ namespace UGF.Module.Assets.Runtime.Tests
     public class TestAssetsModule
     {
         [Test]
+        public void LoadResources()
+        {
+            Object asset1 = Resources.Load("Asset", typeof(Material));
+            Object asset2 = Resources.Load("Asset", typeof(Material));
+
+            Assert.AreEqual(asset1, asset2);
+        }
+
+        [Test]
+        public void UnloadResources()
+        {
+            Object asset1 = Resources.Load("Asset", typeof(Material));
+
+            Resources.UnloadAsset(asset1);
+
+            Object asset2 = Resources.Load("Asset", typeof(Material));
+
+            Assert.AreNotEqual(asset1, asset2);
+        }
+
+        [Test]
         public void Load()
         {
             var application = new ApplicationConfigured(new ApplicationResources
@@ -86,6 +107,51 @@ namespace UGF.Module.Assets.Runtime.Tests
             yield return Resources.UnloadUnusedAssets();
 
             Assert.Null(asset);
+        }
+
+        [Test]
+        public void Track()
+        {
+            var application = new ApplicationConfigured(new ApplicationResources
+            {
+                new ApplicationConfig
+                {
+                    Modules =
+                    {
+                        (IApplicationModuleAsset)Resources.Load("Module", typeof(IApplicationModuleAsset))
+                    }
+                }
+            });
+
+            application.Initialize();
+
+            var module = application.GetModule<IAssetsModule>();
+            object asset1 = module.Load("0", typeof(Material));
+            object asset2 = module.Load("0", typeof(Material));
+
+            Assert.NotNull(asset1);
+            Assert.NotNull(asset2);
+            Assert.AreEqual(asset1, asset2);
+            Assert.IsNotEmpty(module.Tracker.Tracks);
+            Assert.AreEqual(1, module.Tracker.Tracks.Count);
+            Assert.AreEqual(2, module.Tracker.Get("0").Count);
+
+            module.Unload("0", asset1);
+
+            Assert.NotNull(asset1);
+            Assert.NotNull(asset2);
+            Assert.AreEqual(asset1, asset2);
+            Assert.IsNotEmpty(module.Tracker.Tracks);
+            Assert.AreEqual(1, module.Tracker.Tracks.Count);
+            Assert.AreEqual(1, module.Tracker.Get("0").Count);
+
+            module.Unload("0", asset2);
+
+            Assert.AreEqual(null, asset1);
+            Assert.AreEqual(null, asset2);
+            Assert.IsEmpty(module.Tracker.Tracks);
+            Assert.AreEqual(0, module.Tracker.Tracks.Count);
+            Assert.False(module.Tracker.TryGet("0", out _));
         }
     }
 }
