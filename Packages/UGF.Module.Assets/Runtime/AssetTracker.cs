6ace86e7fe4646865edcd0a5b5f39bf7312d22b4
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-27 19:26:58 +0300

    Change tracker

--- Packages/UGF.Module.Assets/Runtime/AssetTracker.cs
+++ Packages/UGF.Module.Assets/Runtime/AssetTracker.cs
@@ -54,6 +54,106 @@ namespace UGF.Module.Assets.Runtime
             m_tracks[id] = track;
         }
 
+        /// <summary>
+        /// Tracks specified asset, increments count when asset already tracked or creates new track of asset otherwise.
+        /// </summary>
+        /// <remarks>
+        /// When asset already tracked, the specified asset must be the same as asset tracked by the specified id.
+        /// </remarks>
+        /// <param name="id">The id of the asset.</param>
+        /// <param name="asset">The asset to track.</param>
+        /// <param name="track">The modified or create asset track as result.</param>
+        /// <returns>Returns True when asset was not already present, otherwise False.</returns>
+        public bool Track(string id, object asset, out AssetTrack track)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+            if (asset == null) throw new ArgumentNullException(nameof(asset));
+
+            if (TryGet(id, out track))
+            {
+                if (track.Asset != asset) throw new ArgumentException($"Asset track not the same as specified asset: track:'{track.Asset}', asset:'{asset}'.");
+
+                track = Increment(id);
+                return false;
+            }
+
+            track = Add(id, asset);
+            track = Increment(id);
+            return true;
+        }
+
+        /// <summary>
+        /// UnTracks specified asset, decrements count when asset already tracked and removes when asset track count reached zero.
+        /// </summary>
+        /// <param name="id">The id of the asset.</param>
+        /// <param name="track">The modified asset track as result.</param>
+        /// <returns>Returns True when asset track count reached zero and was removed, otherwise False.</returns>
+        public bool UnTrack(string id, out AssetTrack track)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+
+            if (TryGet(id, out track))
+            {
+                track = Decrement(id);
+
+                if (track.Zero)
+                {
+                    Remove(id);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public AssetTrack Increment(string id)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+
+            AssetTrack track = Get(id);
+
+            track.Increment();
+
+            Update(id, track);
+
+            return track;
+        }
+
+        public AssetTrack Decrement(string id)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
+
+            AssetTrack track = Get(id);
+
+            track.Decrement();
+
+            Update(id, track);
+
+            return track;
+        }
+
+        public AssetTrack GetByAsset(object asset)
+        {
+            return TryGetByAsset(asset, out AssetTrack track) ? track : throw new ArgumentException($"Asset track not found by the specified asset: '{asset}'.");
+        }
+
+        public bool TryGetByAsset(object asset, out AssetTrack track)
+        {
+            if (asset == null) throw new ArgumentNullException(nameof(asset));
+
+            foreach (KeyValuePair<string, AssetTrack> pair in m_tracks)
+            {
+                if (pair.Value.Asset == asset)
+                {
+                    track = pair.Value;
+                    return true;
+                }
+            }
+
+            track = default;
+            return false;
+        }
+
         public AssetTrack Get(string id)
         {
             return TryGet(id, out AssetTrack asset) ? asset : throw new ArgumentException($"Asset track not found by the specified id: '{id}'.");
@@ -64,6 +164,11 @@ namespace UGF.Module.Assets.Runtime
             if (string.IsNullOrEmpty(id)) throw new ArgumentException("Value cannot be null or empty.", nameof(id));
 
             return m_tracks.TryGetValue(id, out track);
+        }
+
+        public Dictionary<string, AssetTrack>.Enumerator GetEnumerator()
+        {
+            return m_tracks.GetEnumerator();
         }
     }
 }
--- Packages/UGF.Module.Assets/Runtime/IAssetTracker.cs
+++ Packages/UGF.Module.Assets/Runtime/IAssetTracker.cs
@@ -11,6 +11,12 @@ namespace UGF.Module.Assets.Runtime
         bool Remove(string id);
         void Clear();
         void Update(string id, AssetTrack track);
+        bool Track(string id, object asset, out AssetTrack track);
+        bool UnTrack(string id, out AssetTrack track);
+        AssetTrack Increment(string id);
+        AssetTrack Decrement(string id);
+        AssetTrack GetByAsset(object asset);
+        bool TryGetByAsset(object asset, out AssetTrack track);
         AssetTrack Get(string id);
         bool TryGet(string id, out AssetTrack track);
     }
