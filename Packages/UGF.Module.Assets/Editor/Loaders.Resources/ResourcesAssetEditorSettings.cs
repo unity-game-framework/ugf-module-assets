ce48af2d0ef16d900122b0eb8d69be70514066f6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-26 21:56:58 +0300

    Add ResourcesAssetEditorSettings

---
+++ Packages/UGF.Module.Assets/Editor/Loaders.Resources/ResourcesAssetEditorSettings.cs
@@ -0,0 +1,20 @@
+﻿using UGF.CustomSettings.Editor;
+using UnityEditor;
+
+namespace UGF.Module.Assets.Editor.Loaders.Resources
+{
+    public static class ResourcesAssetEditorSettings
+    {
+        public static CustomSettingsEditorPackage<ResourcesAssetEditorSettingsData> Settings { get; } = new CustomSettingsEditorPackage<ResourcesAssetEditorSettingsData>
+        (
+            "UGF.Module.Assets",
+            "ResourcesAssetEditorSettings"
+        );
+
+        [SettingsProvider]
+        private static SettingsProvider GetProvider()
+        {
+            return new CustomSettingsProvider<ResourcesAssetEditorSettingsData>("Project/UGF/Assets Resources", Settings, SettingsScope.Project);
+        }
+    }
+}
