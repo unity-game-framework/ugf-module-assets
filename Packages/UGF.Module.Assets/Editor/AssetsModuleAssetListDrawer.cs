6ec7a556ee64e89732bc3dd89da74b776eb3c657
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-01-26 19:36:40 +0300

    Add assets list in assets module

---
+++ Packages/UGF.Module.Assets/Editor/AssetsModuleAssetListDrawer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UGF.EditorTools.Editor.IMGUI;
+using UnityEditor;
+using UnityEngine;
+
+namespace UGF.Module.Assets.Editor
+{
+    internal class AssetsModuleAssetListDrawer : ReorderableListDrawer
+    {
+        private readonly GUIContent[] m_labels = { GUIContent.none, GUIContent.none };
+
+        public AssetsModuleAssetListDrawer(SerializedProperty serializedProperty) : base(serializedProperty)
+        {
+        }
+
+        protected override void OnDrawElementContent(Rect position, SerializedProperty serializedProperty, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty propertyLoader = serializedProperty.FindPropertyRelative("m_loader");
+            SerializedProperty propertyAsset = serializedProperty.FindPropertyRelative("m_asset");
+
+            float space = EditorGUIUtility.standardVerticalSpacing;
+            float indent = EditorIMGUIUtility.IndentPerLevel;
+
+            var rectLoader = new Rect(position.x, position.y, EditorGUIUtility.labelWidth - space + indent, position.height);
+            var rectAsset = new Rect(rectLoader.xMax + space, position.y, position.width - rectLoader.width, position.height);
+
+            EditorGUI.PropertyField(rectLoader, propertyLoader, GUIContent.none);
+            EditorGUI.PropertyField(rectAsset, propertyAsset, GUIContent.none);
+        }
+
+        protected override float OnElementHeightContent(SerializedProperty serializedProperty, int index)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        protected override bool OnElementHasVisibleChildren(SerializedProperty serializedProperty)
+        {
+            return false;
+        }
+    }
+}
--- Packages/UGF.Module.Assets/Runtime/AssetsModuleAsset.cs
+++ Packages/UGF.Module.Assets/Runtime/AssetsModuleAsset.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UGF.Application.Runtime;
 using UGF.EditorTools.Runtime.IMGUI.AssetReferences;
 using UGF.EditorTools.Runtime.IMGUI.Attributes;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace UGF.Module.Assets.Runtime
 {
@@ -12,17 +12,28 @@ namespace UGF.Module.Assets.Runtime
     {
         [SerializeField] private bool m_unloadTrackedAssetsOnUninitialize = true;
         [SerializeField] private List<AssetReference<AssetLoaderAsset>> m_loaders = new List<AssetReference<AssetLoaderAsset>>();
+        [SerializeField] private List<AssetEntry> m_assets = new List<AssetEntry>();
         [SerializeField] private List<AssetReference<AssetGroupAsset>> m_groups = new List<AssetReference<AssetGroupAsset>>();
-        [AssetGuid(typeof(Object))]
-        [SerializeField] private List<string> m_preload = new List<string>();
-        [AssetGuid(typeof(Object))]
-        [SerializeField] private List<string> m_preloadAsync = new List<string>();
+        [SerializeField, AssetGuid] private List<string> m_preload = new List<string>();
+        [SerializeField, AssetGuid] private List<string> m_preloadAsync = new List<string>();
 
         public bool UnloadTrackedAssetsOnUninitialize { get { return m_unloadTrackedAssetsOnUninitialize; } set { m_unloadTrackedAssetsOnUninitialize = value; } }
         public List<AssetReference<AssetLoaderAsset>> Loaders { get { return m_loaders; } }
         public List<AssetReference<AssetGroupAsset>> Groups { get { return m_groups; } }
+        public List<AssetEntry> Assets { get { return m_assets; } }
         public List<string> Preload { get { return m_preload; } }
         public List<string> PreloadAsync { get { return m_preloadAsync; } }
+
+        [Serializable]
+        public struct AssetEntry
+        {
+            [AssetGuid(typeof(AssetLoaderAsset))]
+            [SerializeField] private string m_loader;
+            [SerializeField, AssetGuid] private string m_asset;
+
+            public string Loader { get { return m_loader; } set { m_loader = value; } }
+            public string Asset { get { return m_asset; } set { m_asset = value; } }
+        }
 
         protected override IApplicationModuleDescription OnBuildDescription()
         {
@@ -41,6 +52,14 @@ namespace UGF.Module.Assets.Runtime
                 IAssetLoader loader = asset.Asset.Build();
 
                 description.Loaders.Add(asset.Guid, loader);
+            }
+
+            for (int i = 0; i < m_assets.Count; i++)
+            {
+                AssetEntry asset = m_assets[i];
+                var info = new AssetInfo(asset.Loader, asset.Asset);
+
+                description.Assets.Add(asset.Asset, info);
             }
 
             for (int i = 0; i < m_groups.Count; i++)
