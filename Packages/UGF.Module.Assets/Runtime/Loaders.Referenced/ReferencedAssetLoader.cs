59c59e585ad97647de312ed5d31f259e4c699686
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-12-23 17:11:08 +0300

    Add ReferencedAssetLoader

---
+++ Packages/UGF.Module.Assets/Runtime/Loaders.Referenced/ReferencedAssetLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Module.Assets.Runtime.Loaders.Referenced
+{
+    public class ReferencedAssetLoader : AssetLoader<ReferencedAssetInfo>
+    {
+        protected override object OnLoad(ReferencedAssetInfo info, string id, Type type, IAssetLoadParameters parameters, IContext context)
+        {
+            return info.Asset;
+        }
+
+        protected override Task<object> OnLoadAsync(ReferencedAssetInfo info, string id, Type type, IAssetLoadParameters parameters, IContext context)
+        {
+            return Task.FromResult<object>(info.Asset);
+        }
+
+        protected override void OnUnload(ReferencedAssetInfo info, string id, object asset, IAssetUnloadParameters parameters, IContext context)
+        {
+        }
+
+        protected override Task OnUnloadAsync(ReferencedAssetInfo info, string id, object asset, IAssetUnloadParameters parameters, IContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
