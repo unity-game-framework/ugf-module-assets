90103d0079ddcb1d9d9a21dae60b49d5e941d63c
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-15 18:45:11 +0300

    Add AssetFolderAsset

---
+++ Packages/UGF.Module.Assets/Editor/AssetFolderAsset.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UGF.Module.Assets.Editor
+{
+    public abstract class AssetFolderAsset : ScriptableObject
+    {
+        [SerializeField] private Object m_folder;
+
+        public Object Folder { get { return m_folder; } set { m_folder = value; } }
+
+        public bool IsValid()
+        {
+            if (m_folder != null)
+            {
+                string path = AssetDatabase.GetAssetPath(m_folder);
+
+                return AssetDatabase.IsValidFolder(path) && OnIsValid();
+            }
+
+            return false;
+        }
+
+        public Type GetAssetType()
+        {
+            return OnGetAssetType();
+        }
+
+        public void Update()
+        {
+            OnUpdate();
+        }
+
+        protected virtual bool OnIsValid()
+        {
+            return true;
+        }
+
+        protected virtual Type OnGetAssetType()
+        {
+            return typeof(Object);
+        }
+
+        protected abstract void OnUpdate();
+    }
+}
---
+++ Packages/UGF.Module.Assets/Editor/AssetFolderAssetEditor.cs
@@ -0,0 +1,28 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UnityEditor;
+
+namespace UGF.Module.Assets.Editor
+{
+    [CustomEditor(typeof(AssetFolderAsset), true)]
+    public class AssetFolderAssetEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_propertyFolder;
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                EditorGUILayout.PropertyField(m_propertyFolder);
+
+                OnDrawGUILayout();
+            }
+        }
+
+        protected virtual void OnDrawGUILayout()
+        {
+        }
+    }
+}
