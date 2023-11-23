152b80dc2d73980b3dba49dc46046e5337c53fc1
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-23 17:20:09 +0300

    Remove AssetFolderAsset

--- Packages/UGF.Module.Assets/Editor/Loaders.Referenced/ReferencedAssetGroupFolderAsset.cs
+++ Packages/UGF.Module.Assets/Editor/Loaders.Referenced/ReferencedAssetGroupFolderAsset.cs
@@ -1,4 +1,5 @@
-﻿using UGF.EditorTools.Runtime.Assets;
+﻿using UGF.Assets.Editor;
+using UGF.EditorTools.Runtime.Assets;
 using UGF.EditorTools.Runtime.Ids;
 using UGF.Module.Assets.Runtime.Loaders.Referenced;
 using UnityEditor;
@@ -7,20 +8,11 @@ using UnityEngine;
 namespace UGF.Module.Assets.Editor.Loaders.Referenced
 {
     [CreateAssetMenu(menuName = "Unity Game Framework/Assets/Referenced Asset Group Folder", order = 2000)]
-    public class ReferencedAssetGroupFolderAsset : AssetFolderAsset
+    public class ReferencedAssetGroupFolderAsset : AssetFolderAsset<ReferencedAssetGroupAsset, Object>
     {
-        [SerializeField] private ReferencedAssetGroupAsset m_group;
-
-        public ReferencedAssetGroupAsset Group { get { return m_group; } set { m_group = value; } }
-
-        protected override bool OnIsValid()
-        {
-            return m_group != null;
-        }
-
         protected override void OnUpdate()
         {
-            m_group.Assets.Clear();
+            Collection.Assets.Clear();
 
             string[] guids = FindAssetsAsGuids();
 
@@ -31,10 +23,10 @@ namespace UGF.Module.Assets.Editor.Loaders.Referenced
 
                 var asset = AssetDatabase.LoadAssetAtPath<Object>(path);
 
-                m_group.Assets.Add(new AssetIdReference<Object>(new GlobalId(guid), asset));
+                Collection.Assets.Add(new AssetIdReference<Object>(new GlobalId(guid), asset));
             }
 
-            EditorUtility.SetDirty(m_group);
+            EditorUtility.SetDirty(Collection);
         }
     }
 }
